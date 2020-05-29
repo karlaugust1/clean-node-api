5855b7724009b536a7e025744a382fa1ce870259
karl-august2011@hotmail.com <karl.harder@roit.com.br>
2020-05-29 20:56:05 -0300

    feat: ensure SignUpController return 400 if no email is provided

--- src/presentations/controllers/signup.ts
+++ src/presentations/controllers/signup.ts
@@ -3,10 +3,17 @@ import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignUpController {
 
-    handle(_httpRequest: HttpRequest): HttpResponse {
-        return {
-            statusCode: 400,
-            body: new MissingParamError("name")
+    handle(httpRequest: HttpRequest): HttpResponse {
+        if (!httpRequest.body.name) {
+            return {
+                statusCode: 400,
+                body: new MissingParamError("name")
+            }
+        } else if (!httpRequest.body.email) {
+            return {
+                statusCode: 400,
+                body: new MissingParamError("email")
+            }
         }
     }
 
