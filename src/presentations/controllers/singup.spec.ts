ffeb422bdc8f08d37a6285a4ac2eb9f307b6bb3a
karl-august2011@hotmail.com <karl.harder@roit.com.br>
2020-05-29 20:56:17 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentations/controllers/singup.spec.ts
+++ src/presentations/controllers/singup.spec.ts
@@ -16,4 +16,19 @@ describe("SignUp Controller", () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError("name"))
     })
+
+    test("Should return 400 if no email is provided", () => {
+        // SUT == System Under Test => Class that we are testing
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                password: "any_password",
+                passwoordConfirmation: "any_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError("email"))
+    })
 })
