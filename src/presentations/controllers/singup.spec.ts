c5b86115e79aae14c70a3e54f37b982d3dfb0a29
karl-august2011@hotmail.com <karl.harder@roit.com.br>
2020-05-29 21:13:01 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentations/controllers/singup.spec.ts
+++ src/presentations/controllers/singup.spec.ts
@@ -9,7 +9,7 @@ describe("SignUp Controller", () => {
             body: {
                 email: "any_email@mail.com",
                 password: "any_password",
-                passwoordConfirmation: "any_password"
+                passwordConfirmation: "any_password"
             }
         }
         const httpResponse = sut.handle(httpRequest)
@@ -24,7 +24,7 @@ describe("SignUp Controller", () => {
             body: {
                 name: "any_name",
                 password: "any_password",
-                passwoordConfirmation: "any_password"
+                passwordConfirmation: "any_password"
             }
         }
         const httpResponse = sut.handle(httpRequest)
@@ -39,11 +39,26 @@ describe("SignUp Controller", () => {
             body: {
                 name: "any_name",
                 email: "any_email@mail.com",
-                passwoordConfirmation: "any_password"
+                passwordConfirmation: "any_password"
             }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError("password"))
+    })
+
+    test("Should return 400 if no password confirmation is provided", () => {
+        // SUT == System Under Test => Class that we are testing
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "any_email@mail.com",
+                password: "any_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
     })
 })
