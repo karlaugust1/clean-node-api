426bbe51c0da16af3d7deae8eb85b9b2116674b4
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-06-11 19:05:45 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest"
+import app from "../config/app"
+
+describe("SignUp Routes", () => {
+    test("Shoud return an account on success", async () => {
+        await request(app)
+            .post("/api/signup")
+            .send({
+                name: "any_name",
+                email: "any_email@mail.com",
+                password: "any_password",
+                passwordConfirmation: "any_password"
+            })
+            .expect(200)
+    })
+})
+
