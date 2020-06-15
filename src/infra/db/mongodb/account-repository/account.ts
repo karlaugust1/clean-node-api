0612e0b8bc33a87ce09f124c17e8f92172c4ed1e
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-06-15 19:18:49 -0300

    feat: ensure MongoHelper has always a connection when getCollection is called

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from "../helpers/mongo-helper";
 export class AccountMongoRepository implements AddAccountRepository {
 
     async add(accountData: AddAccountModel): Promise<AccountModel> {
-        const accounCollection = MongoHelper.getCollection("accounts")
+        const accounCollection = await MongoHelper.getCollection("accounts")
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         const result = await accounCollection.insertOne(accountData)
 
