8d8cc20ccb935df63017ad404b19b868cae7dd8d
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-14 13:46:50 -0300

    feat: ensure SignUpController calls Validation with correct values

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -10,6 +10,7 @@ interface SutTypes {
 const makeEncrypter = (): Encrypter => {
     class EncrypterStub implements Encrypter {
 
+        // eslint-disable-next-line no-unused-vars
         async encrypt(_value: string): Promise<string> {
             return Promise.resolve("hashed_password")
         }
