da8b6b02746acaffeb2b3e390caff6a85a4d038c
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-14 13:50:47 -0300

    test: ensure SignUpController returns 400 if Validation fails

--- src/presentations/controllers/signup/signup.spec.ts
+++ src/presentations/controllers/signup/signup.spec.ts
@@ -218,4 +218,11 @@ describe("SignUp Controller", () => {
         await sut.handle(httpRequest)
         expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
     })
+
+    test("Should return 400 if validation returns an error", async () => {
+        const { sut, validationStub } = makeSut()
+        jest.spyOn(validationStub, "validade").mockReturnValueOnce(new MissingParamError("any_field"))
+        const httpResponse = await sut.handle(makeHttpRequest())
+        expect(httpResponse).toEqual(badRequest(new MissingParamError("any_field")))
+    })
 })
