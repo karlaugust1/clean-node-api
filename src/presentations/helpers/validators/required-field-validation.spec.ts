fa121570a60c564e09ca0bd02438700b68a5a5b7
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-20 10:56:09 -0300

    refactor: move RequiredFieldValidation creation to a method

--- src/presentations/helpers/validators/required-field-validation.spec.ts
+++ src/presentations/helpers/validators/required-field-validation.spec.ts
@@ -1,14 +1,16 @@
 import { RequiredFieldValidation } from "./required-field-validation"
 import { MissingParamError } from "../../errors"
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation("field")
+
 describe("RequiredFiel Validation", () => {
     test("Should return a MissingParamError if validation fails", () => {
-        const sut = new RequiredFieldValidation("field")
+        const sut = makeSut()
         const error = sut.validade({ name: "any_name" })
         expect(error).toEqual(new MissingParamError("field"))
     })
     test("Should not return a MissingParamError if validation succeeds", () => {
-        const sut = new RequiredFieldValidation("field")
+        const sut = makeSut()
         const error = sut.validade({ field: "any_name" })
         expect(error).toBeFalsy()
     })
