5a847a4747f6efcaa16547e6f9a687661bdfb776
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-30 14:25:30 -0300

    feat: add survey composition

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -6,7 +6,7 @@ export const adaptRoute = (controller: Controller) => async (req: Request, res: 
         body: req.body
     }
     const httpResponse: HttpResponse = await controller.handle(httpRequest)
-    if (httpResponse.statusCode === 200) {
+    if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
         res.status(httpResponse.statusCode).send(httpResponse.body)
     } else {
         res.status(httpResponse.statusCode).json({
---
+++ src/main/factories/controllers/add-survey/add-survey-controller-factory.ts
@@ -0,0 +1,11 @@
+import { Controller } from "../../../../presentations/protocols"
+import { makeLogControllerDecorator } from "../../decorators/log-controller-decorator.factory"
+import { makeAddSurveyValidation } from "./add-survey-validation-factory"
+import { makeDbAddSurvey } from "../../usecases/add-survey/db-add-survey-factory"
+import { AddSurveyController } from "../../../../presentations/controllers/survey/add-survey/add-survey-controller"
+
+export const makeAddSurveyController = (): Controller => {
+    const controller = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
+
+    return makeLogControllerDecorator(controller)
+}
