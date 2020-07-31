ca14fa9a863f0485988a7c4f6d37e63847de8b65
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-31 14:35:58 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -13,7 +13,7 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
     async loadByToken(accessToken: string, role?: string): Promise<AccountModel> {
         const token = await this.decrypter.decrypt(accessToken)
         if (token) {
-            const account = await this.loadAccountByTokenRepository.loadByToken(token, role)
+            const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
 
             return account
         }
--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from "supertest"
 import app from "../config/app"
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper"
 import { Collection } from "mongodb"
+import { sign } from "jsonwebtoken"
+import env from "../config/env"
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe("Survey Routes", () => {
     beforeAll(async () => {
@@ -17,6 +20,8 @@ describe("Survey Routes", () => {
     beforeEach(async () => {
         surveyCollection = await MongoHelper.getCollection("surveys")
         await surveyCollection.deleteMany({})
+        accountCollection = await MongoHelper.getCollection("accounts")
+        await accountCollection.deleteMany({})
     })
 
     describe("POST /surveys", () => {
@@ -28,11 +33,43 @@ describe("Survey Routes", () => {
                     answers: [{
                         answer: "Answer 1",
                         image: "http://image-name.com"
-                    },{
+                    }, {
                         answer: "Answer 2"
                     }]
                 })
                 .expect(403)
+        })
+
+        test("Shoud return 204 on add survey with valid accessToken", async () => {
+            const result = await accountCollection.insertOne({
+                name: "any_name",
+                email: "any_email@mail.com",
+                password: "any_password",
+                role: "admin"
+            })
+            const id = result.ops[0]._id
+            const accessToken = sign({ id }, env.jwtSecret)
+            await accountCollection.updateOne({
+                _id: id
+            }, {
+                $set: {
+                    accessToken
+                }
+            })
+
+            await request(app)
+                .post("/api/surveys")
+                .set("x-access-token", accessToken)
+                .send({
+                    question: "any_question",
+                    answers: [{
+                        answer: "Answer 1",
+                        image: "http://image-name.com"
+                    }, {
+                        answer: "Answer 2"
+                    }]
+                })
+                .expect(204)
         })
     })
 })
