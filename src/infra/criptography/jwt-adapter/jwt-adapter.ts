55ff7c89dfe89e2c4dbfe1159f14cded73fa525a
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-31 13:23:27 -0300

    feat: ensure JwtAdpter calls Decrypter with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken"
 import { Encrypter } from "../../../data/protocols/criptograph/encrypter";
+import { Decrypter } from "../../../data/protocols/criptograph/decrypter";
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
 
     constructor(private readonly secret: string) {
     }
@@ -10,6 +11,12 @@ export class JwtAdapter implements Encrypter {
         const accessToken = jwt.sign({ id: value }, this.secret)
 
         return Promise.resolve(accessToken)
+    }
+
+    async decrypt(value: string): Promise<string> {
+        jwt.verify(value, this.secret)
+
+        return Promise.resolve("null")
     }
 
 }
