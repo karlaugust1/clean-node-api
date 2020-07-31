0d2c39276da9e34c1c4917bc3e03817b9708f15c
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-07-31 13:29:10 -0300

    test: ensure JwtAdapter returns a value on success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import jwt from "jsonwebtoken"
 import { JwtAdapter } from "./jwt-adapter"
 
@@ -41,6 +42,12 @@ describe("JWT Adapter", () => {
             const verifySpy = jest.spyOn(jwt, "verify")
             await sut.decrypt("any_token")
             expect(verifySpy).toHaveBeenCalledWith("any_token", "secret")
+        })
+
+        test("Should return a value on verify success", async () => {
+            const sut = makeSut()
+            const value = await sut.decrypt("any_token")
+            expect(value).toBe("any_value")
         })
     })
 })
