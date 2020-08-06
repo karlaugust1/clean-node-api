648763f16dedd188317c524db2586665e68fe17b
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-06 13:05:57 -0300

    feat: ensure LoadSurveysController retusn a list of SurveyModel

--- src/presentations/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentations/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,6 +1,7 @@
 import { SurveyModel, LoadSurveys } from "./load-surveys-controller-protocols"
 import { LoadSurveysController } from "./load-surveys-controller"
 import MockDate from "mockdate"
+import { ok } from "../../../helpers/http/http-helper"
 
 const makeFakeSurveys = (): SurveyModel[] => [{
     id: "any_id",
@@ -59,5 +60,10 @@ describe("LoadSurveys Controller", () => {
         const loadSpy = jest.spyOn(loadSurveysStub, "load")
         await sut.handle({})
         expect(loadSpy).toHaveBeenCalled()
+    })
+    test("Should return 200 on success", async () => {
+        const { sut } = makeSut()
+        const httpResponse = await sut.handle({})
+        expect(httpResponse).toEqual(ok(makeFakeSurveys()))
     })
 })
