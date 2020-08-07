b1925199e8b43144ca8999c094651aa3f76526fc
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-07 20:23:53 -0300

    test: ensure LoadSurveyController returns 204 if no surveys were found

--- src/presentations/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentations/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,7 +1,7 @@
 import { SurveyModel, LoadSurveys } from "./load-surveys-controller-protocols"
 import { LoadSurveysController } from "./load-surveys-controller"
 import MockDate from "mockdate"
-import { ok, serverError } from "../../../helpers/http/http-helper"
+import { ok, serverError, noContent } from "../../../helpers/http/http-helper"
 
 const makeFakeSurveys = (): SurveyModel[] => [{
     id: "any_id",
@@ -65,6 +65,12 @@ describe("LoadSurveys Controller", () => {
         const { sut } = makeSut()
         const httpResponse = await sut.handle({})
         expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+    })
+    test("Should return 204 if LoadSurveys returns empty", async () => {
+        const { sut, loadSurveysStub } = makeSut()
+        jest.spyOn(loadSurveysStub, "load").mockReturnValueOnce(new Promise(resolve => resolve([])))
+        const httpResponse = await sut.handle({})
+        expect(httpResponse).toEqual(noContent())
     })
     test("Shoul return 500 if AddSurvey throws", async () => {
         const { sut, loadSurveysStub } = makeSut()
