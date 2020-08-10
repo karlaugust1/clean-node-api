51ae5d82133bd9b84221f80187eb15f114ccf078
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-10 12:59:55 -0300

    feat: ensure SurveyMongoRepository return a list of Surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,8 +1,10 @@
 import { AddSurveyRepository } from "../../../../data/protocols/db/survey/add-survey-repository";
 import { AddSurveyModel } from "../../../../domain/usecases/add-survey";
 import { MongoHelper } from "../helpers/mongo-helper"
+import { LoadSurveysRepository } from "../../../../data/protocols/db/survey/load-surveys-repository";
+import { SurveyModel } from "../../../../domain/models/survey";
 // eslint-disable-next-line max-len
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
 
     async add(surveyData: AddSurveyModel): Promise<void> {
         const surveyCollection = await MongoHelper.getCollection("surveys")
@@ -10,6 +12,14 @@ export class SurveyMongoRepository implements AddSurveyRepository {
         await surveyCollection.insertOne(surveyData)
 
         return
+    }
+
+    async loadAll(): Promise<SurveyModel[]> {
+        const surveyCollection = await MongoHelper.getCollection("surveys")
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        const surveys: SurveyModel[] = await surveyCollection.find().toArray()
+
+        return surveys
     }
 
 }
