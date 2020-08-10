23cafe9d69896ea70b2a9574bb9bbe098606c86d
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-10 13:11:11 -0300

    feat: ensure load surveys routes returns 403 if no access token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,11 @@
+import { Controller } from "../../../../../presentations/protocols"
+import { makeLogControllerDecorator } from "../../../decorators/log-controller-decorator.factory"
+import { makeDbLoadSurveys } from "../../../usecases/survey/load-surveys/db-load-surveys"
+// eslint-disable-next-line max-len
+import { LoadSurveysController } from "../../../../../presentations/controllers/survey/load-surveys/load-surveys-controller"
+
+export const makeLoadSurveysController = (): Controller => {
+    const controller = new LoadSurveysController(makeDbLoadSurveys())
+
+    return makeLogControllerDecorator(controller)
+}
