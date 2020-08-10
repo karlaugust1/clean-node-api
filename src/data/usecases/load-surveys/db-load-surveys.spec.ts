dbdf2a7d5a899b2803f4fb8dc0ca3852af4b8f4a
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-10 12:51:03 -0300

    feat: ensure DbLoadSurvey throws if LoadSurveysRepository throws

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -58,4 +58,11 @@ describe("DbLoadSurveys", () => {
         const surveys = await sut.load()
         expect(surveys).toEqual(makeFakeSurveys())
     })
+    test("Should throw if LoadSurveysRepository throws", async () => {
+        const { sut, loadSurveysRepositoryStub } = makeSut()
+        // eslint-disable-next-line max-len
+        jest.spyOn(loadSurveysRepositoryStub, "loadAll").mockReturnValueOnce(new Promise((_resolve, reject) => reject(new Error())))
+        const promise = sut.load()
+        await expect(promise).rejects.toThrow()
+    })
 })
