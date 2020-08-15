6ddda0e5e1aa99b761927e008d63948966aed16a
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-15 17:46:44 -0300

    test: ensure save survey result route return 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,31 @@
 import request from "supertest"
 import app from "../config/app"
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper"
+import { Collection } from "mongodb"
+import { sign } from "jsonwebtoken"
+import env from "../config/env"
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+    const result = await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password"
+    })
+    const id = result.ops[0]._id
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({
+        _id: id
+    }, {
+        $set: {
+            accessToken
+        }
+    })
+
+    return accessToken
+}
 
 describe("Survey Routes", () => {
     beforeAll(async () => {
@@ -11,6 +36,13 @@ describe("Survey Routes", () => {
         await MongoHelper.disconnect()
     })
 
+    beforeEach(async () => {
+        surveyCollection = await MongoHelper.getCollection("surveys")
+        await surveyCollection.deleteMany({})
+        accountCollection = await MongoHelper.getCollection("accounts")
+        await accountCollection.deleteMany({})
+    })
+
     describe("PUT /surveys/:surveyId/results", () => {
         test("Shoud return 403 on save survey result without accessToken", async () => {
             await request(app)
@@ -19,6 +51,26 @@ describe("Survey Routes", () => {
                     answer: "any_answer"
                 })
                 .expect(403)
+        })
+        test("Shoud return 200 on save survey result with accessToken", async () => {
+            const accessToken = await makeAccessToken()
+            const res = await surveyCollection.insertOne({
+                question: "any_question",
+                answers: [{
+                    answer: "Answer 1",
+                    image: "http://image-name.com"
+                }, {
+                    answer: "Answer 2"
+                }]
+            })
+            await request(app)
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                .put(`/api/surveys/${res.ops[0]._id}/results`)
+                .set("x-access-token", accessToken)
+                .send({
+                    answer: "Answer 1"
+                })
+                .expect(200)
         })
     })
 })
