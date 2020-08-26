96925d65f3ddc4ceac037f29239c11fbc85363b6
karl-august2011@hotmail.com <karl-august2011@hotmail.com>
2020-08-26 12:32:41 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: "3.0.0",
+    info: {
+        title: "Clean Node API",
+        description: "API com arquitetura limpa para cadastro de enquetes!",
+        version: "1.0.0"
+    }
+}
