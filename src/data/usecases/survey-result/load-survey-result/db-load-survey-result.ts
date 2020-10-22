00f4e325d6724205086a0724ee8bacc74d103fbd
Karl Harder <karl-august2011@hotmail.com>
2020-10-22 10:59:57 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with corrects values

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,17 @@
+import { SurveyResultModel } from "../../../../domain/models/survey-result";
+import { LoadSurveyResult } from "../../../../domain/usecases/survey-result/load-survey-result";
+import { LoadSurveyResultRepository } from "../../../protocols/db/survey-result/load-survey-result-repository";
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+
+    constructor(
+        private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+    ) { }
+
+    async load(surveyId: string): Promise<SurveyResultModel> {
+        await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+
+        return null
+    }
+
+}
