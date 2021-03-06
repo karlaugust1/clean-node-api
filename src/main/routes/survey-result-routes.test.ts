b42a1935d9d9072d3f243337eb74ff7ef06416cc
Karl Harder <karl-august2011@hotmail.com>
2021-03-06 14:43:51 -0300

    test: ensure load survey result returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -80,6 +80,24 @@ describe("Survey Routes", () => {
                 .get("/api/surveys/any_id/results")
                 .expect(403)
         })
+
+        test("Shoud return 200 on load survey result with accessToken", async () => {
+            const accessToken = await makeAccessToken()
+            const res = await surveyCollection.insertOne({
+                question: "any_question",
+                answers: [{
+                    answer: "Answer 1",
+                    image: "http://image-name.com"
+                }, {
+                    answer: "Answer 2"
+                }]
+            })
+            await request(app)
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                .get(`/api/surveys/${res.ops[0]._id}/results`)
+                .set("x-access-token", accessToken)
+                .expect(200)
+        })
     })
 })
 
